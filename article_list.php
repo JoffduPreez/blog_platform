5decd8a79827e7da8e8cc8ce4629890ebca99834
Joffre du Preez <joffredupreez@joffres-m1-mbp.lan>
2021-12-29 17:15:03 -0500

    added new index page

---
+++ article_list.php
@@ -0,0 +1,26 @@
+<?php
+require 'classes/Article.php';
+require 'classes/Database.php';
+
+$conn = Database::getConnection();
+$articles = Article::getAllArticles($conn);
+?>
+<h1>TEST 6</h1>
+
+<?php require 'includes/header.php'; ?>
+<a href="new_article.php">New article</a>
+<?php if (empty($articles)): ?>
+    <p>No articles found.</p>
+<?php else: ?>
+    <ul>
+        <?php foreach ($articles as $article): ?>
+            <li>
+                <article>
+                    <h3><a href="article.php?id=<?= $article['id']; ?>"><?= htmlspecialchars($article['title']); ?></a></h3>
+                    <p><?= htmlspecialchars($article['content']); ?></p>
+                </article>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+<?php require 'includes/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,27 +1,9 @@
-<?php
-require 'classes/Article.php';
-require 'classes/Database.php';
-
-$conn = Database::getConnection();
-$articles = Article::getAllArticles($conn);
-
-?>
-<h1>TEST 1</h1>
+<h1>TEST 16</h1>
 
 <?php require 'includes/header.php'; ?>
-<a href="new_article.php">New article</a>
-<?php if (empty($articles)): ?>
-    <p>No articles found.</p>
-<?php else: ?>
-    <ul>
-        <?php foreach ($articles as $article): ?>
-            <li>
-                <article>
-                    <h3><a href="article.php?id=<?= $article['id']; ?>"><?= htmlspecialchars($article['title']); ?></a></h3>
-                    <p><?= htmlspecialchars($article['content']); ?></p>
-                </article>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
+<h1>Hello There!</h1>
+<h4>Please login or sign up to continue</h4>
+
+<a href="/login.php">Login</a>
+<a href="/register.php">Sign Up</a>
 <?php require 'includes/footer.php'; ?>
